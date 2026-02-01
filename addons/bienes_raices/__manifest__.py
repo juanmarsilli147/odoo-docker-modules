55a7aef0c462357523c6bac11beed26dac31e5d6
Juan Marsilli <juanmarsilli147@gmail.com>
2026-02-01 20:41:37 -0300

    Modulo Bienes Raices - Adicion de Vistas y Modificacion Modelo

--- addons/bienes_raices/__manifest__.py
+++ addons/bienes_raices/__manifest__.py
@@ -8,6 +8,8 @@
     "depends": ["base"],
     "data": [
         "security/ir.model.access.csv",
+        "views/estate_property_views.xml",
+        "views/real_estate_menus.xml",
     ],
     "application": True,
 }
--- addons/bienes_raices/models/estate_property.py
+++ addons/bienes_raices/models/estate_property.py
@@ -1,12 +1,21 @@
 from odoo import models, fields
 
+
 class EstateProperty(models.Model):
     _name = "estate.property"
     _description = "Property"
 
     name = fields.Char(string="Nombre", required=True)
     description = fields.Text(string="Descripción")
-    bedrooms = fields.Integer(string="Camas")
+    active = fields.Boolean(string="Active", default=True)
+    state = fields.Selection([
+        ('new', 'New'),
+        ('offer_received', 'Offer Received'),
+        ('offer_accepted', 'Offer Accepted'),
+        ('sold', 'Sold'),
+        ('canceled', 'Canceled'),
+    ], string="State", default='new')
+    bedrooms = fields.Integer(string="Bed Rooms", default=2)
     living_area = fields.Integer(string="Living Area")
     facades = fields.Integer(string="Facades")
     garden_area = fields.Integer(string="Garden Area")
@@ -16,12 +25,12 @@ class EstateProperty(models.Model):
         ('south', 'South'),
         ('east', 'East'),
         ('west', 'West'),
-    ], string="Garden Orientation")
-    date_availability = fields.Date(string="Date Availability")
+    ], string="Garden Orientation", default='north')
+    date_availability = fields.Date(string="Date Availability", default = lambda self: fields.Date.add(fields.Date.today(), months=3), copy=False)
     garage = fields.Boolean(string="Garage")
     garden = fields.Boolean(string="Garden")
     expected_price = fields.Float(string="Expected Price", required=True)
-    selling_price = fields.Float(string="Selling Price")
+    selling_price = fields.Float(string="Selling Price", readonly=True, copy=False)
     
     
     
